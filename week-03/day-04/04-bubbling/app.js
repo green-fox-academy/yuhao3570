1188f5ac8cdf64c7d4d052b2b9c5b2cda84b82d9
= <Hao_Yu@epam.com>
2019-10-10 16:01:43 +0800

    w3d4 04: giveup on centralized zoom

--- week-03/day-04/04-bubbling/app.js
+++ week-03/day-04/04-bubbling/app.js
@@ -35,13 +35,12 @@ function move(direction) {
 function zoom(direction) {
     if (direction === 'in') {
         size *= 1.2;
-        positionX += Math.round(500 * 0.2);
-        console.log(positionX);
-        positionY += Math.round(320 * 0.2);
+        // positionX += Math.round(positionX * 0.2);
+        // positionY += Math.round(positionY * 0.2);
     } else {
         size *= 0.8;
-        positionX -= Math.round(positionX * 0.2);
-        positionY -= Math.round(positionY * 0.2);
+        // positionX -= Math.round(positionX * 0.2);
+        // positionY -= Math.round(positionY * 0.2);
     }
     imageModification(positionX, positionY, size);
 }
