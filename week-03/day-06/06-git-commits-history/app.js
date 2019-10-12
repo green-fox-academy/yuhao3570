c396e1a642a7c8262cc1a17bd8715329529d3736
= <Hao_Yu@epam.com>
2019-10-12 17:09:50 +0800

    w3d6 donw. optional next 30 commit history undone

--- week-03/day-06/06-git-commits-history/app.js
+++ week-03/day-06/06-git-commits-history/app.js
@@ -1,5 +1,4 @@
 let headers = new Headers()
-headers.set('Authorization', 'Basic ' + btoa('yuhao3570@gmail.com' + ":" + '044da62301f56ad26b9af31602fe767db96ca1de'));
 
 const repos = ['_'];
 getRepo();
@@ -7,17 +6,17 @@ getRepo();
 document.querySelector('button').addEventListener('click', _ => {
     document.querySelector('.container').innerHTML = '';
     let next = popRepo().next()
-    if(next !== undefined){
+    if (next !== undefined) {
         const repo = next.value.name;
         document.querySelector('#title').textContent = repo;
         getCommits(repo);
-    }else{
+    } else {
         document.querySelector('#title').textContent = 'someone';
     }
 });
 
 async function getRepo() {
-    const response = await fetch ('https://api.github.com/orgs/green-fox-academy/repos');
+    const response = await fetch('https://api.github.com/orgs/green-fox-academy/repos');
     const data = await response.json();
     repos[0] = data;
 }
@@ -30,9 +29,7 @@ async function getCommits(repo) {
     let commitHistory = await fetch(`https://api.github.com/repos/green-fox-academy/${repo}/commits`);
     const commits = await commitHistory.json();
 
-    console.log(commits);
-    
-    for(let commit of commits){
+    for (let commit of commits) {
         addCommit(commit.commit);
     }
 }
