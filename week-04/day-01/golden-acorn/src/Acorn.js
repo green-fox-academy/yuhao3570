ce67f5c313ced45ebb39845208527eb8cb22573f
= <Hao_Yu@epam.com>
2019-10-14 17:47:07 +0800

    w4d1 going home~~~ golden-acorn incomplete

---
+++ week-04/day-01/golden-acorn/src/Acorn.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Acorn extends React.Component {
+  constructor(props) {
+      super(props);
+    this.state = {count: 10};
+    this.buy = this.buy.bind(this);
+    this.eat = this.eat.bind(this);
+
+  }
+
+  handleClick(keyPressed) {
+    if (keyPressed === 38) {
+      this.buy();
+    }
+    if (keyPressed === 40) {
+      this.eat();
+    }
+  }
+
+  buy() {
+    this.setState({ count: this.states.count + 1 });
+  }
+  eat() {
+    if(this.state.count !== 0){
+        this.setState({ count: this.states.count - 1}); 
+    }
+  }
+  render() {
+    return (
+    <div>
+      <button className="buy" onClick={this.buy}>Buy One</button>
+      <p className="count">{this.state.count}</p>
+      <button className="eat" onClick={this.eat}>Eat one</button>
+    </div>);
+  }
+}
+
+export default Acorn;
---
+++ week-04/day-01/golden-acorn/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Acorn from './Acorn';
+
+function App() {
+  return (
+    <div className="App">
+    <Acorn/>
+    </div>
+  );
+}
+
+export default App;
