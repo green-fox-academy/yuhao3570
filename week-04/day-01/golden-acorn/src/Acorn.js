6b309ae6d1807b36dfa139309e1c71f814ba09ad
= <Hao_Yu@epam.com>
2019-10-15 11:23:17 +0800

    w4d1 - makeup: golden acron

--- week-04/day-01/golden-acorn/src/Acorn.js
+++ week-04/day-01/golden-acorn/src/Acorn.js
@@ -2,33 +2,35 @@ import React from 'react';
 
 class Acorn extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
     this.state = {count: 10};
     this.buy = this.buy.bind(this);
     this.eat = this.eat.bind(this);
 
   }
 
-  handleClick(keyPressed) {
-    if (keyPressed === 38) {
-      this.buy();
+  handleClick = (event) => {
+    if (event.key === 'ArrowUp') {
+      return this.buy();
     }
-    if (keyPressed === 40) {
-      this.eat();
+    if (event.key === 'ArrowDown') {
+      return this.eat();
     }
   }
 
   buy() {
-    this.setState({count: this.states.count + 1 });
+    this.setState({count: this.state.count + 1 });
   }
   eat() {
     if(this.state.count !== 0){
-        this.setState({count: this.states.count - 1}); 
+        this.setState({count: this.state.count - 1}); 
     }
   }
   render() {
     return (
     <div>
+      {document.addEventListener("keydown", this.handleClick)}
+      
       <button className="buy" onClick={this.buy}>Buy One</button>
       <p className="count">{this.state.count}</p>
       <button className="eat" onClick={this.eat}>Eat one</button>
