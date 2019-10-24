730b0ecf2df755629dd93fdd19bb687b0c1de54f
= <Hao_Yu@epam.com>
2019-10-24 11:58:16 +0800

    w5d4 first-server

---
+++ week-05/day-04/first_server/Server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html')
+});
+
+app.listen(3000);
