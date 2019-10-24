694c73030c678cdeb695567852908985b37f2afa
= <Hao_Yu@epam.com>
2019-10-24 13:46:55 +0800

    w5d4 hello-world

---
+++ week-05/day-04/hello-world/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  res.render('home', {
+      title: 'Hello World!',
+      name: 'hao',
+      company: 'Epam',
+      location: 'shenzhen'
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
