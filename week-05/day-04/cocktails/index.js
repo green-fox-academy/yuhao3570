bee99766b5f1697b38c270e32479e2a2fb554d23
= <Hao_Yu@epam.com>
2019-10-24 17:00:18 +0800

    w5d4 cocktails, css is awfulllllllll

---
+++ week-05/day-04/cocktails/index.js
@@ -0,0 +1,33 @@
+const PORT = 3000;
+const express = require('express');
+const app = express();
+
+const cocktails = [
+    { name: 'GIN FIZZ', price: 1520, contains: ['gin', 'sugar', 'lemon juice', 'soda'], isAlcoholic: true },
+    { name: 'BLOODY MARY', price: 1650, contains: ['vodka', 'tomato juice', 'spices'], isAlcoholic: true },
+    { name: 'SEX ON THE BEACH', price: 1850, contains: ['vodka', 'peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: true },
+    { name: 'CUBA LIBRE', price: 1850, contains: ['rum', 'cola', 'lime juice'], isAlcoholic: true },
+    { name: 'MOJITO', price: 1850, contains: ['rum', 'sugar', 'lime juice', 'soda water'], isAlcoholic: true },
+    { name: 'LONG ISLAND ICE TEA', price: 2450, contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'], isAlcoholic: true },
+    { name: 'VIRGIN MOJITO', price: 990, contains: ['sugar', 'lime juice', 'soda water'], isAlcoholic: false },
+    { name: 'SAFE SEX ON THE BEACH', price: 990, contains: ['peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: false },
+];
+
+const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
+
+const myFilter = (type) => {
+    return cocktails.filter(cocktail => {
+        return cocktail.contains.includes(type);
+    })
+}
+
+app.listen(PORT);
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.render('cocktailView', {
+        cocktailList: req.query.alcohol ? myFilter(req.query.alcohol) : cocktails,
+        alcoholList
+    })
+})
