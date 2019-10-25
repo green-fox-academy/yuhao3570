83e52f5726c28b8d3af6f816949a57923c4b146a
= <Hao_Yu@epam.com>
2019-10-25 15:19:46 +0800

    w5d5 03-reverser & 04-translater

---
+++ week-05/day-05/04-gibberish-translator/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening to PORT: ${PORT}`);
+})
+
+app.post('/translate', (req, res) => {
+  const {text, lang} = req.body;
+  if(!text || !lang) {
+    res.json({
+      "error": "I can't translate that!"
+    })
+  }
+
+  if(lang !== 'en'){
+    res.json({
+      "error": `Can not translate ${lang.toUpperCase()} to Gibberish, please proveide text in English!`
+    })
+  }
+  
+  res.json({
+    "translated": translate(text),
+    "lang": "gibberish"
+  })
+})
+
+function translate(text) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = '';
+  for(let letter of text){
+    if(vowels.includes(letter)){
+      result += (letter + 'l' + letter);
+    }else{
+      result += letter;
+    }
+  }
+  return result;
+}
