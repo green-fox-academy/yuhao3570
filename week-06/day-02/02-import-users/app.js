d50a32baa158e3531650b757efe3e46c4a109074
= <Hao_Yu@epam.com>
2019-10-29 16:43:59 +0800

    w6d2 import-users

--- week-06/day-02/02-import-users/app.js
+++ week-06/day-02/02-import-users/app.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const mysql = require('mysql');
-const PORT = 3000;
-
-const app = express();
-app.use(express.json());
-app.set('view engine', 'ejs');
+const {
+  readFile
+} = require('fs');
 
 const connect = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -12,3 +10,58 @@ const connect = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
+
+const deleteQuery = 'DELETE FROM user WHERE 1=1;';
+const createQuery = 'CREATE TABLE IF NOT EXISTS user( \
+  id INT PRIMARY KEY AUTO_INCREMENT, \
+  prefix VARCHAR(20), \
+  first_name VARCHAR(20), \
+  last_name VARCHAR(20), \
+  address TEXT, \
+  height FLOAT, \
+  bitcoin_address TEXT, \
+  color_preference VARCHAR(7) \
+  );';
+
+const insertionQuery = 'INSERT INTO user VALUES(?, ?, ?, ?, ?, ?, ?, ?);'
+
+const promisedQuery = (sqlString, queryInput='') => {
+  new Promise((resolve, reject) => {
+      connect.query(sqlString, queryInput, (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      });
+  })
+};
+
+// Create table
+(async() => {
+  try{
+    await promisedQuery(createQuery);
+    console.log('user table created');
+  } catch(error){
+    console.log(error)
+  }
+})();
+
+// Insertion
+readFile('./users.csv', 'utf8', async (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+ 
+  const rows = data.split('\n');
+  for (let i = 1; i < rows.length; i++){
+    let detail = rows[i].split(',');
+    try{
+      await promisedQuery(insertionQuery, detail);
+    } catch(error){
+      console.log(error)
+    }
+  }
+  connect.end();
+})
